9dcd76ebb53e93fd68ca76559b88fbceeee376ce
juan cruz <jcdominguez01.jcd@gmail.com>
2023-09-26 22:19:29 -0300

    min/maxXZLimit variables in CameraHandler

--- Assets/Scripts/Camera/CameraHandler.cs
+++ Assets/Scripts/Camera/CameraHandler.cs
@@ -5,20 +5,22 @@ using DG.Tweening;
 
 public class CameraHandler : MonoBehaviour
 {
-    [SerializeField] float dragSpeed = 40; // Aumenta para movimiento más rápido
-    [SerializeField] float tweenDuration = 0.3f; // Reduce para un movimiento más rápido
+    [SerializeField] float dragSpeed = 5; // Velocidad de arrastre de la cámara
+    [SerializeField] float tweenDuration = 0.15f; // Duración del tween
+    [SerializeField] Vector2 minXZLimit; // Límites mínimos en las coordenadas X y Z
+    [SerializeField] Vector2 maxXZLimit; // Límites máximos en las coordenadas X y Z
 
     private Vector2 dragOrigin; // Posición de inicio del arrastre
-    private bool isDragging = false;
 
 
     void Start()
     {
+        minXZLimit = new Vector2(-50f, -100f);
+        maxXZLimit = new Vector2(50f, 100f);
         // Detectar la densidad de píxeles o resolución del dispositivo y ajustar dragSpeed en consecuencia
         float scaleFactor = Screen.dpi / 800f; // Ajusta esto según tus necesidades
         dragSpeed *= scaleFactor;
     }
-
     void Update()
     {
         // Verifica si se toca la pantalla
@@ -26,33 +28,28 @@ public class CameraHandler : MonoBehaviour
         {
             Touch touch = Input.GetTouch(0);
 
-            // Maneja el arrastre solo si estamos haciendo clic en un objeto con el tag "wall"
-            if (!isDragging)
+            // Maneja el arrastre
+            switch (touch.phase)
             {
-                if (touch.phase == TouchPhase.Began)
-                {
-                    Ray ray = Camera.main.ScreenPointToRay(touch.position);
-                    RaycastHit hit;
+                case TouchPhase.Began:
+                    dragOrigin = touch.position;
+                    break;
 
-                    if (Physics.Raycast(ray, out hit) && hit.collider.CompareTag("wall"))
-                    {
-                        isDragging = true;
-                        dragOrigin = touch.position;
-                    }
-                }
-            }
-            else if (touch.phase == TouchPhase.Moved)
-            {
-                Vector2 dragDelta = touch.position - dragOrigin;
-                Vector3 movement = new Vector3(-dragDelta.x * dragSpeed * Time.deltaTime, 0, -dragDelta.y * dragSpeed * Time.deltaTime);
+                case TouchPhase.Moved:
+                    Vector2 dragDelta = touch.position - dragOrigin;
+                    Vector3 movement = new Vector3(-dragDelta.x * dragSpeed * Time.deltaTime, 0, -dragDelta.y * dragSpeed * Time.deltaTime);
 
-                // Usar DOTween para el movimiento suave
-                transform.DOMove(transform.position + movement, tweenDuration);
-                dragOrigin = touch.position;
-            }
-            else if (touch.phase == TouchPhase.Ended || touch.phase == TouchPhase.Canceled)
-            {
-                isDragging = false;
+                    // Calcula la nueva posición después del movimiento
+                    Vector3 newPosition = transform.position + movement;
+
+                    // Aplica restricciones de límites
+                    newPosition.x = Mathf.Clamp(newPosition.x, minXZLimit.x, maxXZLimit.x);
+                    newPosition.z = Mathf.Clamp(newPosition.z, minXZLimit.y, maxXZLimit.y);
+
+                    // Usar DOTween para el movimiento suave
+                    transform.DOMove(newPosition, tweenDuration);
+                    dragOrigin = touch.position;
+                    break;
             }
         }
     }
